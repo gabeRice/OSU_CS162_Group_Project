82b75aecc80bc98ce21ea1d87c4cfa7d541e1b3e
jkbartos <38967534+jkbartos@users.noreply.github.com>
2018-05-04 11:00:43 -0500

    Create Doodlebug.hpp

---
+++ Doodlebug.hpp
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * File: Doodlebug.hpp
+ * Author:
+ * Date:
+ * Description: definition file for the critter class
+*******************************************************************************/
+
+#ifndef DOODLEBUG_HPP
+#define DOODLEBUG_HPP
+
+class Doodlebug : public Critter
+{
+	protected:
+
+	public:
+
+};
+
+
+
+#endif
+
+
+
