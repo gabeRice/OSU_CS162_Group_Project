70d18f5bbf7868eec25312512d32df34ceb9b429
jkbartos <38967534+jkbartos@users.noreply.github.com>
2018-05-04 10:52:46 -0500

    Create Ant.hpp

---
+++ Ant.hpp
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * File: Ant.hpp
+ * Author:
+ * Date:
+ * Description: definition file for the critter class
+*******************************************************************************/
+
+#ifndef ANT_HPP
+#define ANT_HPP
+
+class Ant : public Critter
+{
+	protected:
+
+	public:
+
+};
+
+
+
+#endif
+
+
+
