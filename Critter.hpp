526e89d8bf54f2958a55121cad695b7afef87a25
jkbartos <38967534+jkbartos@users.noreply.github.com>
2018-05-04 10:58:38 -0500

    Create Critter.hpp

---
+++ Critter.hpp
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * File: Critter.hpp
+ * Author:
+ * Date:
+ * Description: definition file for the critter class
+*******************************************************************************/
+
+#ifndef CRITTER_HPP
+#define CRITTER_HPP
+
+class Critter
+{
+	protected:
+
+	public:
+
+};
+
+
+
+#endif
+
